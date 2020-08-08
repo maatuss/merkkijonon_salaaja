486085a81e40611e7c908e77e72dd3f41a069e22
Markus Ollonqvist <markusjuhanio@gmail.com>
2020-08-08 19:44:48 +0300

    fixed main method

--- src/main/java/com/maatus/merkkijonon_salaaja/Paaohjelma.java
+++ src/main/java/com/maatus/merkkijonon_salaaja/Paaohjelma.java
@@ -11,20 +11,21 @@ public class Paaohjelma {
         Salaaja salaaja = new Salaaja();
 
         String merkkijono = "123 merkkijonon salaaja 456";
+        String salattu = salaaja.salaa(merkkijono);
+        String purettu = salaaja.pura(salattu);
 
         long aloitus = System.nanoTime();
         System.out.println("Sana: " + merkkijono);
-        System.out.println("Salattu: " + salaaja.salaa(merkkijono));
-        System.out.println("Purettu: " + salaaja.pura(salaaja.salaa(merkkijono)));
+        System.out.println("Salattu: " + salattu);
+        System.out.println("Purettu: " + purettu);
         System.out.println("");
 
         long lopetus = System.nanoTime();
         long kesto = lopetus - aloitus;
 
         System.out.println("Aikaa meni: " + (kesto / 1000000) + " ms.");
-        
-        ///salaaja.tulostaKaannokset();
 
+        ///salaaja.tulostaKaannokset();
     }
 
 }
